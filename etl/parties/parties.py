6f3952164f1cc74533a2dac890c919edd59e5778
emigar <emiliogarciarabel@gmail.com>
2025-10-13 21:00:46 -0400

    added etl for candidates

--- etl/parties/parties.py
+++ etl/parties/parties.py
@@ -36,7 +36,7 @@ def get_wiki_link(party_name):
     params = {
     "engine": "google",
     "q": query,
-    "api_key": "73df8f2974a001ef1a4440094b1c98c7d2b6c8f04cdefddbb2a76c89bbe27a4e"
+    "api_key": os.environ.get('SERAPI')
     }
     search = GoogleSearch(params)
     results = search.get_dict().get("organic_results", [])
@@ -49,7 +49,7 @@ def get_wiki_link(party_name):
 
 def get_party_info(wiki_link):
     if not wiki_link:
-        return {"summary": None, "position": None, "error": "No link found"}
+        return {"summary": None, "position": None, "ref": None, "error": "No link found"}
 
     headers = {
         "User-Agent": (
@@ -99,7 +99,7 @@ def get_party_info(wiki_link):
     else:
         position = "Not listed."
 
-    return {"summary": summary, "position": position}
+    return {"summary": summary, "position": position, "ref": wiki_link}
 
 def insert_into_table(party_dict):
     database_url = os.environ.get("DATABASE_URL")
@@ -108,7 +108,7 @@ def insert_into_table(party_dict):
         return None
 
     # Ensure required keys exist
-    required_keys = ["name", "position", "summary"]
+    required_keys = ["name", "position", "summary", "ref"]
     if not all(key in party_dict for key in required_keys):
         print("Dictionary is missing required keys")
         return None
@@ -119,14 +119,14 @@ def insert_into_table(party_dict):
         cur = conn.cursor()
 
         insert_query = sql.SQL("""
-            INSERT INTO candidate_data.parties (name, position, summary)
-            VALUES (%s, %s, %s)
+            INSERT INTO candidate_data.parties (name, position, summary, ref)
+            VALUES (%s, %s, %s, %s)
             RETURNING id;
         """)
 
         cur.execute(
             insert_query,
-            (party_dict["name"], party_dict["position"], party_dict["summary"])
+            (party_dict["name"], party_dict["position"], party_dict["summary"], party_dict["ref"])
         )
         party_id = cur.fetchone()[0]
 
@@ -157,13 +157,13 @@ def main():
         if not name:
             continue
         link = get_wiki_link(name)
-        party_info = get_party_info(link) if link else {"summary": "not found", "position": "not found"}
+        party_info = get_party_info(link) if link else {"summary": "not found", "position": "not found", "ref": "not_found"}
         if isinstance(party_info, dict):
             party.update(party_info)
             
         else:
             # fallback if something went wrong
-            party.update({"summary": "not found", "position": "not found"})
+            party.update({"summary": "not found", "position": "not found", "ref": "not_found"})
         insert_into_table(party)
 
             
