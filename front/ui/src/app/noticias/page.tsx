b58e0865c404fe3922505006c5af915a6ea6e6d5
carlaLopez <carla.lopezsanchez20@gmail.com>
2025-12-07 18:03:22 -0500

    news page

--- front/ui/src/app/noticias/page.tsx
+++ front/ui/src/app/noticias/page.tsx
@@ -219,10 +219,11 @@ export default function Noticias(){
                 >
                 
                 <span className={s.name}>{n.name} </span>
-                <span></span>
+                {/* <span className={s.count}>{n.news_json.length}</span> */}
                 
                 <div className={s.extraItems}>
                   
+                  <span className={s.count}>{n.news_json.length}</span>
                   <span
                     className={`${s.caret} ${
                       openIndex === i ? s.open : ""
