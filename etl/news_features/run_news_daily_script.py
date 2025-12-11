86061db20c39a19d4a98d00f4300408194b21947
carlaLopez <carla.lopezsanchez20@gmail.com>
2025-12-10 21:02:25 -0500

    news

--- etl/news_features/run_news_daily_script.py
+++ etl/news_features/run_news_daily_script.py
@@ -2,17 +2,9 @@ import sys
 
 import get_latest_news as news
 
-def main():
-    if len(sys.argv) < 2:
-        print("Usage: daily script")
-        raise SystemExit(2)
 
-    mode = sys.argv[1].lower()
-    if mode == "daily":
-        news.news_daily_script()
-    else:
-        print("Invalid mode. Use daily")
-        raise SystemExit(2)
+def main():
+    news.news_daily_script()
 
 if __name__ == "__main__":
     main()
